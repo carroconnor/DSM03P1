55be657eca59934c3bf6dc488d9653212c6926cf
Carr O'Connor <32774374+carroconnor@users.noreply.github.com>
2024-06-20 03:37:47 +0000

    .

--- url.cpp
+++ url.cpp
@@ -1,31 +1,30 @@
 #include "url.h"
 #include "sstream"
 
-// why is my linking not working? moved to header for now instead
-// url::url(std::string urlString, std::string filename):urlString(urlString), filename(filename)
-// {
-//     try{
-//         http::Request request{urlString};
-//         const auto response = request.send("GET");
-//         std::ofstream file(filename);
-//         file << std::string{response.body.begin(), response.body.end()} << '\n';
-//     } catch(const std::exception &e){
-//         std::cerr << "Request failed, error: " << e.what() << "\n";
-//     }
-// }
+url::url(std::string urlString, std::string filename):urlString(urlString), filename(filename)
+{
+    try{
+        http::Request request{urlString};
+        const auto response = request.send("GET");
+        std::ofstream file(filename);
+        file << std::string{response.body.begin(), response.body.end()} << '\n';
+    } catch(const std::exception &e){
+        std::cerr << "Request failed, error: " << e.what() << "\n";
+    }
+}
 
-// void url::displayFile(int maxLines){
-//     try{
-//         std::stringstream ss;
-//         std::ifstream file(filename);
-//         std::string line;
-//         int count = 0;
-//         while (std::getline(file, line) && (count < maxLines || maxLines == -1)){
-//             ss << line;
-//             count++;
-//         }
-//         std::cout << ss.str() << std::endl;
-//     } catch(const std::exception &e){
-//         std::cerr << "Request failed, error: " << e.what() << "\n";
-//     }
-// }
+void url::displayFile(int maxLines){
+    try{
+        std::stringstream ss;
+        std::ifstream file(filename);
+        std::string line;
+        int count = 0;
+        while (std::getline(file, line) && (count < maxLines || maxLines == -1)){
+            ss << line;
+            count++;
+        }
+        std::cout << ss.str() << std::endl;
+    } catch(const std::exception &e){
+        std::cerr << "Request failed, error: " << e.what() << "\n";
+    }
+}
