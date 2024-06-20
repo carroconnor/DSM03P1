351f0c29d2795255be5cdb059d908b0ed4e2039d
Carr O'Connor <32774374+carroconnor@users.noreply.github.com>
2024-06-20 05:05:10 +0000

    memory bug fixes

--- url.h
+++ url.h
@@ -16,32 +16,34 @@ class url {
         std::string urlString;
 };
 
-// url::url(std::string urlString, std::string filename):urlString(urlString), filename(filename)
-// {
-//     try{
-//         http::Request request{urlString};
-//         const auto response = request.send("GET");
-//         std::ofstream file(filename);
-//         file << std::string{response.body.begin(), response.body.end()} << '\n';
-//     } catch(const std::exception &e){
-//         std::cerr << "Request failed, error: " << e.what() << "\n";
-//     }
-// }
+url::url(std::string urlString, std::string filename):urlString(urlString), filename(filename)
+{
+    try{
+        http::Request request{urlString};
+        const auto response = request.send("GET");
+        std::ofstream file(filename);
+        file << std::string{response.body.begin(), response.body.end()} << '\n';
+        file.close();
+    } catch(const std::exception &e){
+        std::cerr << "Request failed, error: " << e.what() << "\n";
+    }
+}
 
-// void url::displayFile(int maxLines){
-//     try{
-//         std::stringstream ss;
-//         std::ifstream file(filename);
-//         std::string line;
-//         int count = 0;
-//         while (std::getline(file, line) && (count < maxLines || maxLines == -1)){
-//             ss << line << std::endl;
-//             count++;
-//         }
-//         std::cout << ss.str() << std::endl;
-//     } catch(const std::exception &e){
-//         std::cerr << "Request failed, error: " << e.what() << "\n";
-//     }
-// }
+void url::displayFile(int maxLines){
+    try{
+        std::stringstream ss;
+        std::ifstream file(filename);
+        std::string line;
+        int count = 0;
+        while (std::getline(file, line) && (count < maxLines || maxLines == -1)){
+            ss << line << std::endl;
+            count++;
+        }
+        std::cout << ss.str() << std::endl;
+        file.close();
+    } catch(const std::exception &e){
+        std::cerr << "Request failed, error: " << e.what() << "\n";
+    }
+}
 
 #endif
